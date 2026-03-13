64e124154dca43faebe5c7fd97cdb97c27aaa60e
Joey Jackson <joeyjackson4712@gmail.com>
2026-03-13 05:12:28 -0400

    birds theme and readme on adding theme

--- README.md
+++ README.md
@@ -3,6 +3,14 @@ A boids flocking simulation built using p5.js
 
 https://en.wikipedia.org/wiki/Boids
 
+## Adding a theme
+Edit src/Theme.ts
+* Add a new enum to the `theme_t` enum for the new theme
+* Update the `ThemeManager` methods:
+    * For `changeTheme`, add the new theme into the rotation
+    * For `getBackgroundColor`, choose an RGB value to be the background color for the theme
+    * For `getBoidVertices`, return a path of vertices to draw the boid models for the theme
+
 ## Upgrading packages
 NOTE: Keep p5.js on latest version of major 1.X.X release (version 2 is incompatible)
 ```
--- src/Theme.ts
+++ src/Theme.ts
@@ -1,11 +1,12 @@
 export const enum theme_t {
   DEFAULT,
   FISH,
+  BIRDS,
 }
 
 export const changeThemeButton = document.getElementById("changeThemeButton");
 
-const fishVertices = () => {
+const fishVertices: () => Array<[number, number]> = () => {
   const _vertices = [
     [4, 2],
     [5, 4],
@@ -22,7 +23,7 @@ const fishVertices = () => {
     [2, 60],
     [4, 74],
   ];
-  const vertices = [];
+  const vertices: Array<[number, number]> = [];
   vertices.push([0, 0]);
   for (let i = 0; i < _vertices.length; i++) {
     const [x, y] = _vertices[i];
@@ -36,7 +37,58 @@ const fishVertices = () => {
   return vertices;
 }
 
-const triangleVertices = () => {
+const birdsVertices: () => Array<[number, number]> = () => {
+  const _vertices = [
+    [1, 2],
+    [2, 3],
+    [2, 4],
+    [3, 6],
+    [2, 10],
+    [5, 12],
+    [7, 12],
+    [8, 11],
+    [9, 10],
+    [12, 9],
+    [16, 8],
+    [19, 7],
+    [20, 6],
+    [24, 3],
+    [27, 2],
+    [28, 3],
+    [28, 4],
+    [26, 12],
+    [25, 14],
+    [22, 18],
+    [19, 22],
+    [16, 25],
+    [12, 26],
+    [11, 26],
+    [9, 25],
+    [7, 24],
+    [6, 26],
+    [4, 29],
+    [3, 30],
+    [3, 31],
+    [4, 34],
+    [5, 38],
+    [4, 40],
+    [2, 41],
+  ];
+  const vertices: Array<[number, number]> = [];
+  vertices.push([0, 0]);
+  for (let i = 0; i < _vertices.length; i++) {
+    const [x, y] = _vertices[i];
+    vertices.push([x / 24, -y / 36]);
+  }
+  vertices.push([0, -41 / 36]);
+  for (let i = _vertices.length - 1; i >= 0; i--) {
+    const [x, y] = _vertices[i];
+    vertices.push([-x / 24, -y / 36]);
+  }
+  return vertices;
+}
+
+const triangleVertices: () => Array<[number, number]> = () => {
   return [
     [-1, -1],
     [1, -1],
@@ -50,6 +102,9 @@ export class ThemeManager {
   static changeTheme() {
     switch (ThemeManager.currentTheme) {
       case theme_t.FISH:
+        ThemeManager.currentTheme = theme_t.BIRDS;
+        break;
+      case theme_t.BIRDS:
         ThemeManager.currentTheme = theme_t.DEFAULT;
         break;
       case theme_t.DEFAULT:
@@ -64,6 +119,9 @@ export class ThemeManager {
       case theme_t.FISH:
         rgb = [18, 185, 227];
         break;
+      case theme_t.BIRDS:
+        rgb = [135, 206, 235];
+        break;
       case theme_t.DEFAULT:
         rgb = [128, 128, 128];
         break;
@@ -71,10 +129,12 @@ export class ThemeManager {
     return rgb;
   }
 
-  static getBoidVertices() {
+  static getBoidVertices(): Array<[number, number]> {
     switch (ThemeManager.currentTheme) {
       case theme_t.FISH:
-        return fishVertices()
+        return fishVertices();
+      case theme_t.BIRDS:
+        return birdsVertices();
       case theme_t.DEFAULT:
         return triangleVertices();
     }
